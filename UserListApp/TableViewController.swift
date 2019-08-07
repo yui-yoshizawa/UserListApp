c289db92742a1c30f5cfb819afc5b999d1aa1633
yui-yoshizawa <yui.yossy.0114@gmail.com>
2019-08-08 01:29:16 +0900

    完成

--- UserListApp/TableViewController.swift
+++ UserListApp/TableViewController.swift
@@ -10,17 +10,20 @@ import UIKit
 
 class TableViewController: UITableViewController {
     
-    var selectNum: Int = 0
+    
+    var selectNum: Int = 0    // // 遷移先に値を渡す時につかう
     
 
    
 
-
+    // セクションの数いくつにしますか〜？
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1    // ここ0にしたらセル表示されなくなった。
     }
 
+    
+    // セルの数いくつにしますか〜？
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return 14
@@ -31,21 +34,22 @@ class TableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        let select = indexPath.row
+        let select = indexPath.row    // select に選択したインデックス番号を代入
         
-        selectNum = select
+        selectNum = select    // selectNum に上記を代入
         
-        performSegue(withIdentifier: "showData", sender: nil)    // 画面遷移したい場所でこのコードを記述。
+        performSegue(withIdentifier: "showData", sender: nil)    // 画面遷移したい場所でこのコードを記述。Identifier を変える。
     }
     
     
     // segue遷移前動作
+    // セグエ実行前処理 / セグエの identifier 確認 / 遷移先ViewController の取得
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard segue.identifier == "showData", let vc = segue.destination as? ViewController else {
             return
         }
         
-        vc.select = selectNum
+        vc.select = selectNum    // 遷移先の select に selectNum を代入
     }
     
 
